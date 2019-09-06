8dd0dca90ebf3cd883c78c372a54abe6a1930616
taichi <taichi730@gmail.com>
2019-09-06 17:23:08 +0900

    fixed the issue output gotes to X when ENTRY is 1

--- tbcm_mux.sv
+++ tbcm_mux.sv
@@ -3,7 +3,7 @@ module tbcm_mux #(
   parameter   type  DATA_TYPE     = logic [WIDTH-1:0],
   parameter   int   ENTRIES       = 2,
   parameter   bit   ONE_HOT       = 1,
-  localparam  int   INDEX_WIDTH   = $clog2(ENTRIES),
+  localparam  int   INDEX_WIDTH   = (ENTRIES >= 2) ? $clog2(ENTRIES) : 1,
   localparam  int   SELECT_WIDTH  = (ONE_HOT) ? ENTRIES : INDEX_WIDTH
 )(
   input   logic [SELECT_WIDTH-1:0]  i_select,
@@ -12,25 +12,30 @@ module tbcm_mux #(
 );
   logic [INDEX_WIDTH-1:0] index;
 
-  assign  index   = get_index(i_select);
   assign  o_data  = i_data[index];
+  if (ENTRIES == 1) begin
+    assign  index = 0;
+  end
+  else if (ONE_HOT) begin
+    assign  index = get_index(i_select);
+  end
+  else begin
+    assign  index = i_select;
+  end
 
   function automatic logic [INDEX_WIDTH-1:0] get_index(
     logic [SELECT_WIDTH-1:0]  select
   );
     logic [INDEX_WIDTH-1:0] index;
-    if (ONE_HOT) begin
-      for (int i = 0;i < INDEX_WIDTH;++i) begin
-        logic [ENTRIES-1:0] temp;
-        for (int j = 0;j < ENTRIES;++j) begin
-          temp[j] = select[j] & j[i];
-        end
-        index[i]  = |temp;
+
+    for (int i = 0;i < INDEX_WIDTH;++i) begin
+      logic [ENTRIES-1:0] temp;
+      for (int j = 0;j < ENTRIES;++j) begin
+        temp[j] = select[j] & j[i];
       end
+      index[i]  = |temp;
     end
-    else begin
-      index = select;
-    end
+
     return index;
   endfunction
 endmodule
